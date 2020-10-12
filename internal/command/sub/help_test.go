f991c0baca77057889d69f249a136937c3c46394
Jeff Schwab <jeff@unbuggy.com>
2020-10-12 14:00:46 -0400

    Check return status in sub.Help test

--- internal/command/sub/help_test.go
+++ internal/command/sub/help_test.go
@@ -1,16 +1,43 @@
 package sub_test
 
-import "testing"
+import (
+	"testing"
 
-// To test:
-//
-//  geode help -- returns zero
-//  geode help knowncommand -- returns zero
-//  geode help unknowncommand -- returns non-zero
-//  geode help multiple commands -- returns non-zero
-//
-// All of the above should print non-empty output.
+	"github.com/jeffs/geode/internal/command/sub"
+)
 
-func TestHelp(t *testing.T) {
-	t.Error("TBD")
+// TestHelpGood checks that sub.Help returns nil on valid input.
+func TestHelpGood(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+	}{
+		{"no args", []string{}},
+		{"known topic", []string{"help"}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if err := sub.Help(tc.args); err != nil {
+				t.Fatalf("got %q, want nil", err)
+			}
+		})
+	}
+}
+
+// TestHelpGood checks that sub.Help returns an error on invalid input.
+func TestHelpBad(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+	}{
+		{"unknown topic", []string{"nonesuch"}},
+		{"too many topics", []string{"help help"}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if err := sub.Help(tc.args); err == nil {
+				t.Fatal("got nil, want error")
+			}
+		})
+	}
 }
