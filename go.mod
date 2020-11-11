875c88472c55aae7900b07a102c8ead57c3e7381
Jeff Schwab <jeff@unbuggy.com>
2020-11-11 16:21:12 -0500

    Expand Dockerfile template with values from TOML

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jeffs/geode
 
 go 1.15
+
+require github.com/BurntSushi/toml v0.3.1
--- internal/cli/dockerfile.go
+++ internal/cli/dockerfile.go
@@ -4,22 +4,41 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
+	"text/template"
+
+	"github.com/BurntSushi/toml"
 )
+
+type config struct {
+	Base string
+	Name string
+	User string
+
+	Locale   string
+	TimeZone string
+}
 
 func Dockerfile(args []string) error {
 	if len(args) != 1 {
 		return errors.New("expected exactly one profile")
 	}
 
-	profile := args[0]
+	var cfg config
+	if _, err := toml.DecodeFile(path.Join(args[0], "dockerfile.toml"), &cfg); err != nil {
+		return err
+	}
 
-	bytes, err := ioutil.ReadFile(path.Join(profile, "Dockerfile"))
+	bytes, err := ioutil.ReadFile(path.Join(args[0], "Dockerfile"))
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(string(bytes))
+	tpl := template.Must(template.New("Dockerfile").Parse(string(bytes)))
+	if err := tpl.Execute(os.Stdout, &cfg); err != nil {
+		return fmt.Errorf("can't generate Dockerfile: %w\n", err)
+	}
 
 	return nil
 }
