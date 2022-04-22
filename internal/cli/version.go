6656f1571c5bb4daee3d9281601ff7a6701e2b0c
Jeff Schwab <jeff@unbuggy.com>
2022-04-22 12:46:29 -0400

    wip

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,16 +2,14 @@ package cli
 
 import (
 	"errors"
-	"os"
-
-	"github.com/jeffs/geode/internal/docker"
+	"fmt"
 )
 
-func Dockerfile(args []string) error {
+func Version(args []string) error {
 	if len(args) != 0 {
 		return errors.New("unexpected argument")
 	}
 
-	fmt.Println("v0.1.1");
-	return nil;
+	fmt.Println("v0.1.1")
+	return nil
 }
